3e0510427115352d56923d5ef0f255427014a850
Renan Lovo Boni <126094373+Meeps0@users.noreply.github.com>
2024-08-14 21:22:05 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,43 @@
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+const informacoes = document.querySelectorAll(".informacoes")
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener ("click", () => {
+        desativarBotaoSelecionado();
+
+        botao.classList.add("selecionado");
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+
+        esconderInformacoesAtivas();
+
+        mostrarInformacoes(indice);
+
+    })
+})
+
+function mostrarInformacoes(indice) {
+    informacoes[indice].classList.add("ativa");
+}
+
+function esconderInformacoesAtivas() {
+    const informacaoAtiva = document.querySelector(".informacoes.ativa");
+    informacaoAtiva.classList.remove("ativa");
+}
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add("ativa");
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector(".selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
